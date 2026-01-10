78fb0ddde1f6f6371e2ee0ef7882b277cffce753
Pss Powers <sam@psspowers.com>
2026-01-10 15:58:55 +0700

    Deleted mockPartners.ts

--- src/data/index.ts
+++
@@ -1,7 +0,0 @@
-export { mockPartners } from './mockPartners';
-export { mockAccounts } from './mockAccounts';
-export { mockContacts } from './mockContacts';
-export { mockOpportunities } from './mockOpportunities';
-export { mockProjects } from './mockProjects';
-export { mockActivities } from './mockActivities';
-export { mockRelationships } from './mockRelationships';
