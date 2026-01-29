521a0fd2323bdc63427dcdb88b7bede6602014ad
Pss Powers <sam@psspowers.com>
2026-01-29 23:32:09 +0700

    Updated ZaapTwo.tsx

--- src/pages/ZaapTwo.tsx
+++ src/pages/ZaapTwo.tsx
@@ -4,14 +4,14 @@ import { Header, BottomNav } from '@/components/crm';
 
 const ZaapTwo: React.FC = () => {
   return (
-    <div className="min-h-screen bg-slate-50 dark:bg-slate-900 flex flex-col overflow-y-auto">
+    <div className="min-h-screen bg-slate-50 dark:bg-slate-900 flex flex-col">
       <Header
         onQuickAdd={() => {}}
         activeTab="tasks"
         onSearchClick={() => {}}
       />
 
-      <main className="flex-1 p-4 lg:p-8 pb-24 lg:pb-8">
+      <main className="flex-1 p-4 lg:p-8 pb-24 lg:pb-8 overflow-y-auto">
         <div className="max-w-7xl mx-auto w-full">
           <ZaapScreenTwo />
         </div>
